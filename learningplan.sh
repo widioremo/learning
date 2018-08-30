e5a70b81711516f24b94eb6a97f329c5259756f3
widioremo <widioremo@gmail.com>
2018-08-30 21:21:02 +0300

    first commit of list

---
+++ learningplan.sh
@@ -0,0 +1,11 @@
+# Stuff I need to learn
+
+- Microservices
+    - https://medium.com/@ssola/building-microservices-with-python-part-i-5240a8dcc2fb?email=widioremo%40gmail.com&g-recaptcha-response=03AHqfIOnmGmvEAkuA5kHWjOiRghFWeMuTEPojPC3KDIpMnMrHxSADR1Wk7_OwbTrDN4u4fM9JwT-x1XcZ32oypnveNUwPZSj0RusofFQEphkH1grj3Aby4d6gkJOrhUkj2e06VNXFzEUET8awTa4rSgQw6UNmBjx-5t4A6UXruqnRThaoT_1nXndPvSoMilpTDm9TKdDBFPUbE1mJ65CTMnlFWUrq0h6p4G21Et4E-VB8xyD5afXsg9jvintgFGiSdNQCGtjCSKuDM42HZaa3MbbrA2avafmMTw
+- Parallel Programming in Python
+    - https://chryswoods.com/parallel_python/index.html
+- Python Patterns
+    - https://python-3-patterns-idioms-test.readthedocs.io/en/latest/PatternConcept.html
+- Algorithms
+- CI/CD
+ 
